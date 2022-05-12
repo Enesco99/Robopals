8279d99852abd4b6ce22317784573a54f2c2a344
Enesco99 <amehenejo@yahoo.com>
2022-05-12 13:14:37 +0100

    xxx

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Ameh Enejo',
+    username: 'Enesco',
+    email: 'amehenejo@yahoo.com'
+  },
+  {
+    id: 2,
+    name: 'Kate Ameh',
+    username: 'Kate',
+    email: 'catherine24@yahoo.com'
+  },
+  {
+    id: 3,
+    name: 'Ojene Ameh',
+    username: 'Ojae',
+    email: 'ojae@yahoo.com'
+  },
+  {
+    id: 4,
+    name: 'Ameh Steve',
+    username: 'Steve',
+    email: 'amehsteve@yahoo.com'
+  },
+  {
+    id: 5,
+    name: 'Ene Echioda',
+    username: 'Linda',
+    email: 'Linda@yahoo.com'
+  },
+  {
+    id: 6,
+    name: 'Ambrose Echioda',
+    username: 'Aj',
+    email: 'aj@yahoo.com'
+  },
+  {
+    id: 7,
+    name: 'Enekhole Echioda',
+    username: 'Kholes',
+    email: 'kholes@yahoo.com'
+  },
+  {
+    id: 8,
+    name: 'Nansat Lengmang',
+    username: 'fowl',
+    email: 'debbie@yahoo.com'
+  },
+  {
+    id: 9,
+    name: 'Jessie Nyiyongo',
+    username: 'Jessie',
+    email: 'Jessie@yahoo.com'
+  },
+  {
+    id: 10,
+    name: 'David Lirfa',
+    username: 'Shegsbaba',
+    email: 'Shegsbaba@yahoo.com'
+  }
+];
